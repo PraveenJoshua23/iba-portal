fc4be328f2e79b72f807da4af4a9476b7f8fd3cf
JohnThomasAlex <johnthomasalex@gmail.com>
2023-11-30 01:23:30 +0530

    commit new codes

--- src/app/lessons.route.ts
+++ src/app/lessons.route.ts
@@ -1,6 +1,5 @@
 import { Route } from '@angular/router';
 import { LessonsComponent } from './pages/lessons/lessons.component';
-import { PlyrModule } from 'ngx-plyr';
 
 export const LESSON_ROUTE: Route[] = [
     {
